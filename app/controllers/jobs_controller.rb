4c1ddba9e1699e530fef3987f4e523621b4a4cd5
Ines Daniel <i.danie8@gmail.com>
2018-11-05 20:38:17 -0600

    make jobs model, make restful actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,48 @@
 class JobsController < ApplicationController
   def index
-    @jobs = jobs_payload
+    @jobs = Job.all
+    render json: {jobs: @jobs}
+  end
+
+  def my_jobs
+    @jobs = current_user.jobs + Job.where(provider_id: current_user.id)
+    render json: {jobs: @jobs}
   end
 
   def new
     render 'new.html.erb'
+  end
+
+  def create
+    @job = Job.new(
+      requested_time: params[:requested_time],
+      user_id: current_user.id,
+      status: "posted"
+      )
+    @job.save
+
+    render json: {job: @job}
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    @job.update(
+      requested_time: params[:requested_time] || @job.requested_time,
+      start_time: params[:start_time] || @job.start_time,
+      end_time: params[:end_time] || @job.end_time,
+      provider_id: params[:provider_id] || @job.provider_id,
+      status: params[:status] || @job.status
+      )
+
+    render json: {job: @job}
+    
+  end
+
+  def show
+    @job = Job.find(params[:id])
+
+    render json: {job: @job}
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_011738) do
+ActiveRecord::Schema.define(version: 2018_11_06_014413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "provider_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "requested_time"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
