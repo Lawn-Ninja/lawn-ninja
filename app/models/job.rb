ba71d8f52dc851fcdd8307dcbf060b5563f457b2
joshsarna <joshsarna@gmail.com>
2018-11-19 13:48:55 -0800

    pseudo-sanitize zip codes before sending db query

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,11 +9,14 @@ class Job < ApplicationRecord
     zip_codes = HTTP.get("https://www.zipcodeapi.com/rest/#{ENV['API_KEY']}/radius.json/#{provider_zip}/#{radius_miles}/mile?minimal").body.to_s.delete('{\"zip_codes\":[').delete(']}').split(',')
 
     zip_codes.each do |zc|
-      users = User.where("zip_code = '#{zc}'")
-      users.each do |user|
-        user.jobs.each do |job|
-          if job.status == "posted"
-            jobs << job
+      int_zc = zc.to_i
+      if int_zc > 0 && int_zc < 100000
+        users = User.where("zip_code = '#{int_zc.to_s}'")
+        users.each do |user|
+          user.jobs.each do |job|
+            if job.status == "posted"
+              jobs << job
+            end
           end
         end
       end
