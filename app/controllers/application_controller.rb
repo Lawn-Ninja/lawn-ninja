b66caf63c283a53a8c8cd480fba4a059e8811e63
Andrew Basner <andrewbasner@gmail.com>
2019-01-07 21:33:17 -0600

    add current provider helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :set_headers
   protect_from_forgery with: :null_session
-
-  def set_headers
-  #   if session[:jwt]
-  #     @jwt = session[:jwt]
-  #     Unirest.default_header("Authorization", "Bearer #{session[:jwt]}")
-  #   else
-  #     Unirest.clear_default_headers()
-  #   end
-  end
 
   def current_consumer
     if request.headers['Authorization'].present?
@@ -21,7 +11,24 @@ class ApplicationController < ActionController::Base
           true,
           { algorithm: 'HS256' }
         )
-        Consumer.find_by(id: decoded_token[0]["consumer"])
+        Consumer.find_by(id: decoded_token[0]['consumer'])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  def current_provider
+    if request.headers['Authorization'].present?
+      token = request.headers['Authorization'].split(' ').last
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: 'HS256' }
+        )
+        Provider.find_by(id: decoded_token[0]['provider'])
       rescue JWT::ExpiredSignature
         nil
       end
@@ -29,9 +36,16 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_consumer
+  helper_method :current_provider
 
   def authenticate_consumer
     unless current_consumer
+      render json: {}, status: :unauthorized
+    end
+  end
+
+  def authenticate_provider
+    unless current_provider
       render json: {}, status: :unauthorized
     end
   end
