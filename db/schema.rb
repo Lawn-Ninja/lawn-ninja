97f2dc4568b75fe3a31d0ea12208240ab76c5767
coryrgriffith <corygriffith9@gmail.com>
2019-01-07 20:28:54 -0600

    change user_id column in jobs table to consumer_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_013443) do
+ActiveRecord::Schema.define(version: 2019_01_08_022459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_01_08_013443) do
   end
 
   create_table "jobs", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "provider_id"
     t.datetime "start_time"
     t.datetime "end_time"
@@ -38,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_013443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "requested_time"
+    t.integer "consumer_id"
   end
 
   create_table "providers", force: :cascade do |t|
