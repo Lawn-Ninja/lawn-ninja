0200548804fc11a7f3f2427e718266b03282d303
joshsarna <joshsarna@gmail.com>
2019-01-11 09:16:01 -0800

    clean up jobs_near_me logic

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,22 +2,25 @@ class Job < ApplicationRecord
   belongs_to :consumer
   validates :requested_time, presence: true
 
-  def self.jobs_near_me(provider_zip, current_consumer_id)
+  def self.jobs_near_me(provider_zip)
     jobs = []
     radius_miles = 5
 
-    zip_codes = HTTP.get("https://www.zipcodeapi.com/rest/#{ENV['API_KEY']}/radius.json/#{provider_zip}/#{radius_miles}/mile?minimal").body.to_s.delete('{\"zip_codes\":[').delete(']}').split(',')
+    request_url = "https://www.zipcodeapi.com/rest/#{ENV['API_KEY']}/radius.json/#{provider_zip}/#{radius_miles}/mile?minimal"
+
+    zip_codes_raw = HTTP.get(request_url)
+    zip_codes = JSON.parse zip_codes_raw.body, symbolize_names: true
+
+    p zip_codes
 
     zip_codes.each do |zc|
       int_zc = zc.to_i
       if int_zc > 0 && int_zc < 100000
         consumers = Consumer.where("zip_code = '#{int_zc.to_s}'")
         consumers.each do |consumer|
-          if consumer.id != current_consumer_id
-            consumer.jobs.each do |job|
-              if job.status == "posted"
-                jobs << job
-              end
+          consumer.jobs.each do |job|
+            if job.status == "posted"
+              jobs << job
             end
           end
         end
